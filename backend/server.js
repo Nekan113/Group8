6f6b6b5aaedbbfe9e175faf5b4b5754bf4e7d718
Lam Nguyen <s3989101@rmit.edu.vn>
2026-04-07 02:27:28 +0700

    Setup mongodb

--- backend/server.js
+++ backend/server.js
@@ -14,3 +14,8 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+require("dotenv").config();
+const connectDB = require("./src/config/db");
+
+connectDB();
