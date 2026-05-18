c510fb29bc4dd669c46ad2f034f8a2810f6173de
Htun_aung <s4026719@rmit.edu.vn>
2026-05-18 15:03:38 +0700

    Frontend

---
+++ frontend/src/pages/profile/ProfilePage.service.js
@@ -0,0 +1,8 @@
+import { profileService } from '../../services/profile.service';
+
+export const fetchProfile   = ()           => profileService.getProfile();
+export const saveProfile    = (data)       => profileService.updateProfile(data);
+export const savePassword   = (data)       => profileService.changePassword(data);
+export const uploadAvatar   = (file)       => profileService.uploadAvatar(file);
+export const depositWallet  = (amount)     => profileService.deposit(amount);
+export const subscribePremium = ()         => profileService.subscribe();
